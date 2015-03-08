82fb34032f5e0bfe9ac86854977be6be48ead963
huanglei <hhuangllei@qq.com>
2015-03-08 11:45:33 +0800

    db.py update _Engine _DbCtx

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -7,6 +7,34 @@ Database operation module
 '''
 
 class Dict(dict):
+    '''
+    Simple dict but support access as x.y style.
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
+    '''
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
@@ -22,4 +50,35 @@ class Dict(dict):
         self[key] = value
 
 def next_id(t = None):
+    pass
+
+# 数据库引擎对象
+class _Engine(object):
+    def __init__(self, connect):
+        self._connect = connect
+    def connect(self):
+        return self._connect()
     
+engine = None
+
+# 持有数据库连接的上下文对象
+class _DbCtx(threading.local):
+    def __init__(self):
+        self.connection = None
+        self.transactions = 0
+
+    def is_init(self):
+        return not self.connection is None
+
+    def init(self):
+        self.connection = _LasyConnection()
+        self.tranctions = 0
+
+    def cleanup(self):
+        self.connection.cleanup()
+        self.connection = None
+
+    def cursor(self):
+        return self.connection.cursor()
+
+_db_ctx = _DbCtx()
